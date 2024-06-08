eb9a4c00fb88882b782c4ac31940276dfe8aad54
Faisal Farooq <dfaisal59@gmail.com>
2024-06-08 23:30:22 +0530

    refactor: Enable email verification for user registration

--- Server/src/app.js
+++ Server/src/app.js
@@ -11,7 +11,7 @@ app.use(cookieParser());
 app.use(express.static("public"));
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "*", // allow to server to accept request from different origin
     credentials: true,
   })
 );
