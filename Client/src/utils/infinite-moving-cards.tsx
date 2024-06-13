b9a0abda55293e3424508f8421646cc3a90f0919
Faisal Farooq <dfaisal59@gmail.com>
2024-06-13 22:05:17 +0530

    refactor: Add padding to InfiniteMovingCards component

--- Client/src/utils/infinite-moving-cards.tsx
+++ Client/src/utils/infinite-moving-cards.tsx
@@ -80,7 +80,7 @@ export const InfiniteMovingCards = ({
       <ul
         ref={scrollerRef}
         className={cn(
-          " flex min-w-full shrink-0 gap-4  w-max flex-nowrap",
+          " flex min-w-full shrink-0 gap-4 py-4 w-max flex-nowrap",
           start && "animate-scroll ",
           pauseOnHover && "hover:[animation-play-state:paused]"
         )}
